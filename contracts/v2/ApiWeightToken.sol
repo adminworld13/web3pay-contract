6b1a8a2bb860b97367a2373d45ae7d569e919352
adminworld13 <128709352+adminworld13@users.noreply.github.com>
2023-03-25 08:58:41 +0000

    Update ApiWeightToken.sol

--- contracts/v2/ApiWeightToken.sol
+++ contracts/v2/ApiWeightToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.4; 1
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
